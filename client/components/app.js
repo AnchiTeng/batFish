73465afda81f057ce3393a1c920042286fc8f6c6
Anchi Teng <anchi1009@gmail.com>
2021-09-23 15:20:11 +0800

    2nd commit

---
+++ client/components/app.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>hey here</h1>
+    )
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 
 
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/",(req,res)=>{
-    res.send("This is working");
+    res.send("This is from server");
     return res;
 })
+// app.get('/home', (req, res) => {
+//     res.sendFile(path.resolve(__dirname + '/public/index.html'))
+//   })
 
 app.listen(PORT)
